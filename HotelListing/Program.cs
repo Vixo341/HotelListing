319a685892a925dfdf27bc9a0ee29d3896b62882
blashot <blashot@DESKTOP-OR8TRAU>
2022-01-24 11:42:36 +0100

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,59 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy", builder =>
+    builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.File(path: "c:\\hotellistings\\logs\\log-.txt",
+    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{level:u3}] {Message:lj}{NewLine}{Exception}",
+    rollingInterval: RollingInterval.Day,
+    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+    );
+
+try
+{
+    Log.Information("Application is starting");
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+    app.UseCors("CorsPolicy");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+
+    Log.Fatal(ex, "Application failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
